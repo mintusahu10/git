4c3b6bdab5cdca6b4fdb397ef882fa4d8e06c364
mintusahu10 <lavank076@example.com>
2023-07-17 18:09:38 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,52 @@
+// start pattern
+
+// console.log("hello");
+// let n=5;
+// let a="";
+// for(let i=1;i<=n;i++){
+//     for(j=0;j<i;j++){
+//         a +="*";
+//     }
+//     a +="\n";
+// }
+// console.log(a);
+
+
+//factorial 
+// let number = 5;
+// let fact=1;
+// for(let i=1;i<=number;i++){
+//     fact*=i;
+// }
+// console.log(fact);
+
+
+//sum of digits  in array format
+
+// let array=[1,2,3,4,5];
+// sum=0;
+// for(let i=0;i<array.length;i++){
+//     sum += array[i];
+// }
+//  console.log(sum);
+
+
+
+// checking number positive ,negative or zero
+
+const number = 5;
+
+// check if number is greater than 0
+if (number > 0) {
+    console.log("The number is positive");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+// if number is less than 0
+else {
+     console.log("The number is negative");
+}
